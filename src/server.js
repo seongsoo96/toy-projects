1678684b81d1c9cfaf2b1089b6e6c804874cb3cc
seongsoo96 <seongsoo.dev@gmail.com>
2021-06-30 15:08:07 +0900

    request

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = () => console.log("Somebody is trying to go home.")
+
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
+
+
