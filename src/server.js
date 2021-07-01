dc328057981d6649783d95c8693dd43a54c48672
seongsoo96 <seongsoo.dev@gmail.com>
2021-07-01 16:45:04 +0900

    #3 done

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = () => console.log("Somebody is trying to go home.")
+const home = (req, res) => {
+    console.log("I will respond.");
+    return res.send("hello");
+};
 
-app.get("/", handleHome);
+const login = (req, res) => {
+    return res.send("login");
+};
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+app.use(logger)
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () => 
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 
