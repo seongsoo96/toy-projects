450814705e31f37fdf280ad56970080ed398ce7f
seongsoo96 <seongsoo.dev@gmail.com>
2022-02-28 12:12:10 +0900

    test git commit

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<div className="container">
 			<Head>
-				<title>Create Next App</title>
+				<title>Create Next App dd</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
