736577a027506a18219e8297d159081e6da56369
seongsoo96 <seongsoo.dev@gmail.com>
2022-05-19 23:21:19 +0900

    css -> scss 변경

--- order-plz/src/App.js
+++ order-plz/src/App.js
@@ -1,7 +1,29 @@
-import './App.css';
+import 'normalize.css'
+import './App.scss';
 
 function App() {
-  return
+
+  
+  return (
+    <>
+      <wrapper>
+        <h1>메뉴 적기</h1>
+        <div>
+          <span>SYS</span><input type="text" /><br />
+          <span>PSS</span><input type="text" /><br />
+          <span>KSW</span><input type="text" /><br />
+          <span>JHS</span><input type="text" /><br />
+          <span>CKC</span><input type="text" /><br />
+          <span>LHH</span><input type="text" /><br />
+          <span>ABC</span><input type="text" /><br />
+          <span>LSS</span><input type="text" /><br />
+          <span>KMK</span><input type="text" /><br />
+          <span>LNE</span><input type="text" /><br />
+        </div>
+      </wrapper>
+      
+    </>
+  );
 }
 
 export default App;
