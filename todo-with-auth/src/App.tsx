5725e55f46bb4cb6ee1632784b5f3f1f960f77ed
seongsoo96 <gasmathdog@gmail.com>
2022-08-31 20:20:20 +0900

    feat: add Route

--- todo-with-auth/src/App.tsx
+++ todo-with-auth/src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import Dashboard from './components/dashboard/Dashboard';
+
 function App() {
   return (
-    <div className="App">
-      <h3>Todo App</h3>
-    </div>
+    <>
+      <BrowserRouter>
+        <h3>Todo App</h3>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
