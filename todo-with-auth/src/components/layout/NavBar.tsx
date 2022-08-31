8ae5c7201cd5f007ca005640b25ab1442c02c050
seongsoo96 <gasmathdog@gmail.com>
2022-08-31 22:17:51 +0900

    feat: Implement NavBar component

--- todo-with-auth/src/components/layout/NavBar.tsx
+++ todo-with-auth/src/components/layout/NavBar.tsx
@@ -1,16 +1,144 @@
-import React from 'react';
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
-import NavItems from './NavItems';
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
-export default function NavBar() {
+interface Props {
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = [
+  {
+    name: 'SignOut',
+    href: '/signin',
+  },
+  {
+    name: 'SignUp',
+    href: '/signup',
+  },
+  {
+    name: 'SignIn',
+    href: '/signin',
+  },
+];
+
+export default function NavBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography
+        component={Link}
+        to="/"
+        variant="h6"
+        sx={{
+          my: 2,
+          display: 'block',
+          textDecoration: 'none',
+          boxShadow: 'none',
+          color: '#000',
+        }}>
+        Todo App
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem
+            key={item.name}
+            component={Link}
+            to={item.href}
+            disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <nav>
-      <nav>
-        <Link to="/">
-          <h3>TodoApp</h3>
-        </Link>
-        <NavItems />
-      </nav>
-    </nav>
+    <div>
+      <Box sx={{ display: 'flex' }}>
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component={Link}
+              to="/"
+              sx={{
+                flexGrow: 1,
+                display: { xs: 'none', sm: 'block' },
+                textDecoration: 'none',
+                boxShadow: 'none',
+                color: '#fff',
+              }}>
+              Todo App
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item.name}
+                  component={Link}
+                  to={item.href}
+                  sx={{ color: '#fff' }}>
+                  {item.name}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}>
+            {drawer}
+          </Drawer>
+        </Box>
+        <Toolbar />
+      </Box>
+    </div>
   );
 }
