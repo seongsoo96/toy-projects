cbb1ba2447bef38b2025509e80202d22d1ebb1e0
seongsoo96 <gasmathdog@gmail.com>
2022-09-02 00:27:48 +0900

    feat: Init setting of react-redux, redux-thunk

--- todo-with-auth/src/index.tsx
+++ todo-with-auth/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+import rootReducer from './modules';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
