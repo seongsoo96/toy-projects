ea32c103326b22c96609788827e80f19bd0ff7c6
ssl <ssl@sslui-MacBookPro.local>
2023-01-25 14:49:51 +0900

    Introduction to express

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -1,27 +1,24 @@
-import express from "express";
-import morgan from "morgan";
+import express from "express"
+import morgan from "morgan"
 
-const PORT = 4000;
+const PORT = 4000
 
-const app = express();
-const logger = morgan("dev");
+const app = express()
+const logger = morgan("dev")
 
 const home = (req, res) => {
-    console.log("I will respond.");
-    return res.send("hello");
-};
-
+  console.log("I will respond")
+  return res.send("hello")
+}
 const login = (req, res) => {
-    return res.send("login");
-};
+  return res.send("login")
+}
 
 app.use(logger)
-app.get("/", home);
-app.get("/login", login);
+app.get("/", home)
+app.get("/login", login)
 
-const handleListening = () => 
-    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`)
 
-app.listen(PORT, handleListening);
-
-
+app.listen(PORT, handleListening)
