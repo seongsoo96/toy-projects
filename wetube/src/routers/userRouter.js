87c3320e5d452bbdef0b987893c9969d78a80d73
ssl <ssl@sslui-MacBookPro.local>
2023-01-31 17:26:02 +0900

    Practice how to get or post using fake db data

--- wetube/src/routers/userRouter.js
+++ wetube/src/routers/userRouter.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { edit, remove, logout } from "../controllers/userController"
-import { see } from "../controllers/videoController"
+import { watch } from "../controllers/videoController"
 
 const userRouter = express.Router()
 
 userRouter.get("/logout", logout)
 userRouter.get("/edit", edit)
 userRouter.get("/remove", remove)
-userRouter.get(":id", see)
+userRouter.get(":id", watch)
 
 export default userRouter
--- wetube/src/server.js
+++ wetube/src/server.js
@@ -6,14 +6,13 @@ import userRouter from "./routers/userRouter"
 
 const PORT = 4000
 
-console.log(process.cwd())
-
 const app = express()
 const logger = morgan("dev")
-app.use(logger)
 
 app.set("view engine", "pug")
 app.set("views", process.cwd() + "/src/views")
+app.use(logger)
+app.use(express.urlencoded({ extended: true }))
 app.use("/", globalRouter)
 app.use("/videos", videoRouter)
 app.use("/users", userRouter)
