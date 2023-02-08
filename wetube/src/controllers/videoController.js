9b80b603eba867efa5542d82f56c0227b338c0fa
ssl <ssl@sslui-MacBookPro.local>
2023-02-08 17:06:08 +0900

    delete mongodb

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -59,3 +59,9 @@ export const postUpload = async (req, res) => {
     })
   }
 }
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params
+  await Video.findByIdAndDelete(id)
+  return res.redirect("/")
+}
