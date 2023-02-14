56587c0e37632c3104b7973ed7191820434f7637
ssl <ssl@sslui-MacBookPro.local>
2023-02-14 16:51:08 +0900

    Create Account

--- wetube/src/controllers/userController.js
+++ wetube/src/controllers/userController.js
@@ -1,4 +1,32 @@
-export const join = (req, res) => res.send("Join")
+import User from "../models/User"
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" })
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, password2, location } = req.body
+  const pageTitle = "Join"
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
+    })
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] })
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken",
+    })
+  }
+
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  })
+  return res.redirect("/login")
+}
 export const edit = (req, res) => res.send("Edit")
 export const remove = (req, res) => res.send("Remove")
 export const login = (req, res) => res.send("Login")
